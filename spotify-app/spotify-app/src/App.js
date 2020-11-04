fdde43a96cfc5641807883905677b82e77c90c44
Amit Kumar <besttechamit@gmail.com>
2020-11-04 22:11:16 +0530

    Basic login integration with the spotify developer done.

--- spotify-app/spotify-app/src/App.js
+++ spotify-app/spotify-app/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify.js';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    
+    console.log("The token is 👉", hash);
+
+    if(_token){
+      setToken(_token)
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+    {token ? <h1>I am logged in</h1>:<Login />}
     </div>
   );
 }
