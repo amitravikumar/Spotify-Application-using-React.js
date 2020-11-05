826f84566371df5f9c6856ac6b5c4ea5a20ec827
Amit Kumar <besttechamit@gmail.com>
2020-11-06 00:22:45 +0530

    Application almost completed

--- spotify-app/spotify-app/src/App.js
+++ spotify-app/spotify-app/src/App.js
@@ -6,43 +6,64 @@ import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
 import { useDataLayerValue } from './DataLayer';
 
-const spotify = new SpotifyWebApi();
+const s = new SpotifyWebApi();
+
 function App() {
-  const [{ user, token }, dispatch] = useDataLayerValue();
+  const [{ token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
+    // Set token
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
-    if(_token){
+    if (_token) {
+      s.setAccessToken(_token);
+
       dispatch({
-        type: 'SET_TOKEN',
-        token: _token
-      })
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
-      spotify.setAccessToken(_token);
-
-      spotify.getMe().then((user) => {
+      s.getPlaylist("15ngsvOmlTkARCg7ipoNvG").then((response) =>
         dispatch({
-          type: 'SET_USER',
-          user: user
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
         })
-      })
+      );
 
-      spotify.getUserPlaylists().then((playlists) => {
+      s.getMyTopArtists().then((response) =>
         dispatch({
-          type: 'SET_PLAYLISTS',
-          playlists: playlists
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
         })
-      })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
     }
-  }, []);
-
+  }, [token, dispatch]);
 
   return (
     <div className="app">
-    {token ? <Player spotify = {spotify}/>:<Login />}
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
     </div>
   );
 }
--- spotify-app/spotify-app/src/reducer.js
+++ spotify-app/spotify-app/src/reducer.js
@@ -4,32 +4,63 @@ export const initialState = {
     playing: false,
     item: null,
     // ---->>>>>>>To be removed before the deployment to the firebase server
-    token: 'BQBHMbbbCWaIdEkGbC-VfY8_nqOfFnQgmgOAItY8qV2zc3',
+    //token: 'BQBHMbbbCWaIdEkGbC-VfY8_nqOfFnQgmgOAItY8qV2zc3',
 }
 
-const reducer = (state, action) =>{
+const reducer = (state, action) => {
     console.log(action);
-
-    //Action -> type, [payload]
-    switch(action.type) {
-        case 'SET_USER':
+    switch (action.type) {
+      case "SET_USER":
         return {
-            ...state,
-            user: action.user
-        }
-        case 'SET_TOKEN':
-            return{
-                ...state,
-                token: action.token
-            }
-        case 'SET_PLAYLISTS': 
-            return {
-                ...state,
-                playlists: action.playlists
-            }
-        default:
-            return state;
+          ...state,
+          user: action.user,
+        };
+  
+      case "SET_PLAYING":
+        return {
+          ...state,
+          playing: action.playing,
+        };
+  
+      case "SET_ITEM":
+        return {
+          ...state,
+          item: action.item,
+        };
+  
+      case "SET_DISCOVER_WEEKLY":
+        return {
+          ...state,
+          discover_weekly: action.discover_weekly,
+        };
+  
+      case "SET_TOP_ARTISTS":
+        return {
+          ...state,
+          top_artists: action.top_artists,
+        };
+  
+      case "SET_TOKEN":
+        return {
+          ...state,
+          token: action.token,
+        };
+  
+      case "SET_SPOTIFY":
+        return {
+          ...state,
+          spotify: action.spotify,
+        };
+  
+      case "SET_PLAYLISTS":
+        return {
+          ...state,
+          playlists: action.playlists,
+        };
+      default:
+        return state;
     }
-}
+  };
+  
 
 export default reducer;
